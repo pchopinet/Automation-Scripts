e29e56db1b5e188f5fb4cb3b8102bfe833e3824e
Pierre Chopinet <pierre.chopinet97@gmail.com>
2018-03-20 20:07:55 +0100

    add ssh cluster

--- network/ssh.sh
+++ network/ssh.sh
@@ -1,5 +1,17 @@
 
-getFiles(){
-    scp -r $1:$2 $3
+ssh-get-files(){
+	scp -r $1:$2 $3
+}
+
+
+ssh-command-cluster(){
+	filename="$1"
+	username="$2"
+	command="$3"
+	while read -r line
+	do
+		ip="$line"
+		ssh $username@$ip -q $command
+	done < "$filename"
 }
 
